8e1d0063de873061ae6d477c187e6f227810dcd2
ddyj <1362335858@qq.com>
2020-03-18 17:03:42 +0800

    '001'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/pages/home/home.vue'
+import list from '@/pages/list/list.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
+    }, {
+      path: '/list',
+      name: 'list',
+      component: list
     }
   ]
 })
